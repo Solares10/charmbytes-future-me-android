6d140344edc396ee16883d57e26186f1f666ae92
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:27:37 -0700

    updated view models

--- app/src/main/java/com/charmbytes/futureme/viewmodel/InboxViewModel.kt
+++ app/src/main/java/com/charmbytes/futureme/viewmodel/InboxViewModel.kt
@@ -1,3 +1,21 @@
 package com.charmbytes.futureme.viewmodel
 import androidx.lifecycle.ViewModel
-class InboxViewModel: ViewModel()
+import androidx.lifecycle.viewModelScope
+import com.charmbytes.futureme.data.CapsuleRepository
+import com.charmbytes.futureme.model.Capsule
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class InboxViewModel(
+    private val repository: CapsuleRepository
+) : ViewModel() {
+
+    val allLetters = repository.allLetters
+
+    fun refreshFromServer(token: String) {
+        viewModelScope.launch {
+            repository.fetchLettersFromServer(token)
+        }
+    }
+}
+
