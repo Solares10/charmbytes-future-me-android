40fdb5b9e429f757a447cf08a3682cde8314d815
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:27:07 -0700

    updated viewmodels

--- app/src/main/java/com/charmbytes/futureme/viewmodel/ComposeViewModel.kt
+++ app/src/main/java/com/charmbytes/futureme/viewmodel/ComposeViewModel.kt
@@ -1,3 +1,23 @@
 package com.charmbytes.futureme.viewmodel
 import androidx.lifecycle.ViewModel
-class ComposeViewModel: ViewModel()
+import androidx.lifecycle.viewModeScope
+import com.charmbytes.futureme.data.CapsuleRepository
+import com.charmbytes.futureme.model.Capsule
+import kotlin.coroutines.launch
+
+class ComposeViewModel(
+    private val repository: CapsuleRepository
+): ViewModel() {
+
+    fun sendLetter(token: String, letter: Capsule) {
+        viewModelScope.launch {
+            repository.sendLetterToServer(token, letter)
+        }
+    }
+
+    fun saveLetterLocally(letter: Capsule) {
+        viewModelScope.launch {
+            repository.insertLetter(letter)
+        }
+    }
+}
