11a49cdcb3ab4afc81b6160bb6cacc1e30e27162
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:26:07 -0700

    Updated Capsule: defined letter (id, title, message, dates (created, delivered), isDelivered)

--- app/src/main/java/com/charmbytes/futureme/data/model/Capsule.kt
+++ app/src/main/java/com/charmbytes/futureme/data/model/Capsule.kt
@@ -1,2 +1,27 @@
 package com.charmbytes.futureme.data.model
-data class Capsule(val title:String = "", val message:String = "")
+
+import com.google.gson.annotations.SerializedName
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "letters")
+data class Capsule (
+    @SerializedName("_id")
+    @PrimaryKey val id:String = "",
+
+    val title:String? = "",
+
+    val message:String? = "",
+
+    @SerializedName("createdAt")
+    val createdAt:Date? = null,
+
+    val delivered:Boolean = false,
+
+    @SerializedName("deliveredAt")
+    val deliveredAt:Date? = null,
+
+    )
+
+
