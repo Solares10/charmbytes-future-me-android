691914d94d38a1aa7a4e16e2300f86ad27fd85ba
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:27:23 -0700

    updated viewmodels

--- app/src/main/java/com/charmbytes/futureme/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/charmbytes/futureme/viewmodel/DetailViewModel.kt
@@ -1,3 +1,28 @@
 package com.charmbytes.futureme.viewmodel
 import androidx.lifecycle.ViewModel
-class DetailViewModel: ViewModel()
+import androidx.lifecycle.viewModelScope
+import com.charmbytes.futureme.data.CapsuleRepository
+import com.charmbytes.futureme.model.Capsule
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class DetailViewModel(
+    private val repository: CapsuleRepository
+): ViewModel() {
+
+    private val _letter = MutableStateFlow<Capsule?>(null)
+    val letter: StateFlow<Capsule?> = _letter
+
+    fun loadLetter(id: String) {
+        viewModelScope.launch {
+            _letter.value = repository.getLetterByID(id)
+        }
+    }
+
+    fun deleteLetter(id: String) {
+        viewModelScope.launch {
+            repository.deleteLetter(id)
+        }
+    }
+}
