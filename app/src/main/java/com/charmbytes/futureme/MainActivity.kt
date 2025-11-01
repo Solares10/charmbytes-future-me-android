dd9d7539cbdc479347a00852dfc5364834f2847d
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:27:55 -0700

    updated mainActivity

--- app/src/main/java/com/charmbytes/futureme/MainActivity.kt
+++ app/src/main/java/com/charmbytes/futureme/MainActivity.kt
@@ -1,3 +1,19 @@
 package com.charmbytes.futureme
 import android.os.Bundle
-class MainActivity { }
+
+class MainActivity: Application() {
+    lateinit var repository: CapsuleRepository
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        val db = AppDatabase.getDatabase(this)
+        val api = Retrofit.Builder()
+            .baseUrl("https://future-me-api.charmbytes.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build
+            .create(CapsulesApi::class.java)
+
+        repository = CapsuleRepository(db.capsuleDao(), api)
+    }
+}
