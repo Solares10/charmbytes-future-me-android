7d2a0d94b65df8101c773bfcb7e2936a007a36f8
thy-hoang <thyh2319@gmail.com>
2025-11-01 00:24:02 -0700

    Updated AppDatabase

--- app/src/main/java/com/charmbytes/futureme/data/local/AppDatabase.kt
+++ app/src/main/java/com/charmbytes/futureme/data/local/AppDatabase.kt
@@ -1,3 +1,36 @@
 package com.charmbytes.futureme.data.local
 import androidx.room.Database
-abstract class AppDatabase
+import android.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import android.content.Context
+import com.charmbytes.futureme.data.local.model.Capsule
+
+@Database (
+    entities = [Letter::class], // in a table
+    version = 1
+    exportSchema = false
+)
+
+abstract class AppDatabase:RoomDatabase() {
+    abstract fun capsuleDao(): CapsuleDao // link to DAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? null;
+
+        fun getDatabase(context:Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "future_script_database"    // database file name
+                );
+                .fallbacktoDestructiveMigration() //resets databse if schema changes (remove in prod)
+                .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
